83d2c24408b054bd09d06ab46bf87333618c85b2
Abduino <abdrehmanredi0@gmail.com>
2022-09-13 15:21:44 -0300

    serial monitor initialized

--- envsystem.py
+++ envsystem.py
@@ -8,4 +8,21 @@ import pandas as pd
 import plotly.graph_objects as go
 import numpy as np 
 import random
-import pygal 
+import pygal
+
+
+arduino = serial.Serial(port='COM3', baudrate=9600, timeout=50)
+while True:
+    data = arduino.readline().decode('ascii')
+    rows = data.split(',')
+
+    #query = "INSERT INTO smart_agri (soil_moisture,water_level,temperature,humidity,light,pressure ,gas,altitudevar,rain) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s)"
+    vsoil_moisture= rows[0]
+    vwater_level= 10
+    vtemperature=rows[1]
+    vhumidity=rows[2]
+    vlight=rows[3]
+    vpressure=rows[5]
+    vgas=rows[4]
+    valtitude=rows[6]
+    vrain=10
