83c43cad73c5523fbc67aa1971f977ad1fce01c3
Abduino <abdrehmanredi0@gmail.com>
2022-09-13 15:28:10 -0300

    python side finished

--- envsystem.py
+++ envsystem.py
@@ -16,7 +16,7 @@ while True:
     data = arduino.readline().decode('ascii')
     rows = data.split(',')
 
-    #query = "INSERT INTO smart_agri (soil_moisture,water_level,temperature,humidity,light,pressure ,gas,altitudevar,rain) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s)"
+
     vsoil_moisture= rows[0]
     vwater_level= 10
     vtemperature=rows[1]
@@ -26,3 +26,56 @@ while True:
     vgas=rows[4]
     valtitude=rows[6]
     vrain=10
+
+    with placeholder.container():
+
+        st.write("---")
+        
+        fig = go.Figure()
+        fig.add_trace(go.Indicator(
+            value = int(float(vsoil_moisture)),
+            title = {'text': "Soil Moisture"},
+            delta = {'reference': 50},
+            gauge = {'axis': {'range': [None, 1000]}},
+            domain = {'row': 0, 'column': 0}))
+        fig.add_trace(go.Indicator(
+            value = int(float(vhumidity)),
+            title = {'text': "Humidity"},
+            delta = {'reference': 50},
+            gauge = {'axis': {'range': [None, 100]}},
+            domain = {'row': 0, 'column': 1}))
+        fig.add_trace(go.Indicator(
+            value = int(float(vtemperature)),
+            title = {'text': "Tempreture"},
+            delta = {'reference': 50},
+            gauge = {'axis': {'range': [None, 100]}},
+            domain = {'row': 0, 'column': 2}))
+        fig.add_trace(go.Indicator(
+            value = int(float(vlight)),
+            title = {'text': "Light"},
+            delta = {'reference': 50},
+            gauge = {'axis': {'range': [None, 1000]}},
+            domain = {'row': 1, 'column': 0}))
+        fig.add_trace(go.Indicator(
+            value = int(float(vpressure)),
+            title = {'text': "Presure"},
+            delta = {'reference': 50},
+            gauge = {'axis': {'range': [None, 5000]}},
+            domain = {'row': 1, 'column': 1}))
+        fig.add_trace(go.Indicator(
+            value = int(float(vrain)),
+            title = {'text': "Rain"},
+            delta = {'reference': 50},
+            gauge = {'axis': {'range': [None, 100]}},
+            domain = {'row': 1, 'column': 2}))
+        fig.update_layout(
+            title_font_size=40,
+            grid = {'rows': 2, 'columns': 3, 'pattern': "independent"},
+            template = {'data' : {'indicator': [{  
+                'mode' : "number+delta+gauge",
+                'delta' : {'reference': 50}}]
+                                     }})
+        fig.update_yaxes(automargin=True)
+        st.plotly_chart(fig)
+        time.sleep(5)
+    
