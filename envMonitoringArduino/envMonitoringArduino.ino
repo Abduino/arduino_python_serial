cb8fda1a1ceb026c6948ac581b756b74ec4d91e1
Abduino <abdrehmanredi0@gmail.com>
2022-09-13 15:12:37 -0300

    environmental variables initialized

--- envMonitoringArduino/envMonitoringArduino.ino
+++ envMonitoringArduino/envMonitoringArduino.ino
@@ -5,6 +5,22 @@ Adafruit_BMP085 bmp;
 dht DHT;
 
 
+int moisture_sensor_pin = A0;
+int light_sensor_pin = A1;
+int gas_sensor_pin = A2;
+float moisture_sensor_value=10.0;
+float temperature_sensor_value=10.0;
+float humidity_sensor_value=10.0;
+float light_sensor_value=10.0;
+float gas_sensor_value;
+float air_pressure_sensor_value=10;
+float sealevel_pressure_sensor_value=10;
+float altitude_sensor_value=10;
+float altitude_sensor_value_2=10;
+float Water_level = 10.0,pressure = 10.0, altitudevar = 10.0,rain=10.0;
+
+
+
 void loop() 
 {
    
