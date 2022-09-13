1052680ba592c9465d8370fa8653fb745ae62954
Abduino <abdrehmanredi0@gmail.com>
2022-09-13 15:08:54 -0300

    python file added

---
+++ envsystem.py
@@ -0,0 +1,11 @@
+#Importing Libraries
+import serial
+import time
+import mysql.connector
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np 
+import random
+import pygal 
